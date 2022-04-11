ff26671604fd34182be8cdb888d32cab45cf8750
eu <eugen_hhh@yahoo.com>
2022-04-11 11:59:45 +0300

    finish classifiers

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -10,6 +10,11 @@
     
 import sys
 from time import time
+
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.neighbors import NearestNeighbors, KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -25,6 +30,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+clf1 = DecisionTreeClassifier(min_samples_split=40)
+rand1 = RandomForestClassifier()
+knear = KNeighborsClassifier()
+adaboost = AdaBoostClassifier()
+
+
+def train_and_classify(clf):
+    t0 = time()
+    # < your clf.fit() line of code >
+    # len(features_train[0]
+    clf.fit(features_train, labels_train)
+    print("Training Time:", round(time() - t0, 3), "s")
+    t0 = time()
+    # < your clf.fit() line of code >
+    results = clf.predict(features_test)
+    print("Predicting Time:", round(time() - t0, 3), "s")
+    print("Accuracy:", clf.score(features_test, labels_test))
+    print("num of features:", len(features_train[0]))
+
+
+train_and_classify(adaboost)
 
 #########################################################
 
